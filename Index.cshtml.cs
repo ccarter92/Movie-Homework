6e125bda49d22cfeed0e59962b7b0b49f65013e7
ccarter92 <54338971+ccarter92@users.noreply.github.com>
2019-10-02 09:09:58 -0500

    Add files via upload

---
+++ Index.cshtml.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace MoviesHW.Pages
+{
+    public class IndexModel : PageModel
+    {
+        public string[] MyMovies { get; set; }
+        
+        public void OnGet()
+        {
+            MyMovies = new string[]
+            {
+                "Friday","Next Friday","Friday After Next"
+            };
+        }
+    }
+}
